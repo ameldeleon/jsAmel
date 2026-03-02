806e3152102e259cf7817403e3626a8e6e09e0ed
ameldeleon <deleonamel@gmail.com>
2026-03-02 19:30:55 -0400

    updated while number to 7

--- Amel/4-ControlFlow/4-while.js
+++ Amel/4-ControlFlow/4-while.js
@@ -3,8 +3,8 @@ for (let i = 0; i <= 5; i++) {
 }
 
 let i = 0;
-while (i <=5) {
-    if (i <= 5) {
+while (i <= 7) {
+    if (i <= 7) {
         if (i % 2 !== 0) console.log(i);
         i++;
     }
