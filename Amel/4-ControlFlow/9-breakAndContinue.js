b33be5e53707d54456bf1e5816d6f7bca70e034d
ameldeleon <deleonamel@gmail.com>
2026-04-15 12:52:59 -0400

    updated break and for loops

---
+++ Amel/4-ControlFlow/9-breakAndContinue.js
@@ -0,0 +1,11 @@
+let i = 0;
+while (i < 10) { 
+    //if (i  === 5) break; // break will stop the loop when i is 5
+    if (i%2 === 0)
+        i++;
+        continue; // continue will skip the current iteration when i is even
+    }
+
+console.log(i);
+    i++;
+    
