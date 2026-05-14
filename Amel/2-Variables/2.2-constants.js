8ced957215981ec0e01c16b6d93a6acaee18e33f
ameldeleon <deleonamel@gmail.com>
2026-05-14 18:11:43 -0400

    created file for next section

---
+++ Amel/2-Variables/2.2-constants.js
@@ -0,0 +1,18 @@
+let name = 'Amel';
+// console.log(name)
+
+//Variables cannot be a reserved keyword
+//Name should be meaningful
+//Cannot start with a number
+//Cannot contain a space or hyphen (-)
+//Are case-sensitive
+
+let firstName = 'Amel';
+let lastName = 'De Leon';
+
+console.log(firstName, lastName)
+
+//Can declare using comma (let firstName = 'Amel', lastName = "De Leon")
+//Industry standard nowadays is let firstName = 'Amel';
+//                              let lastName = 'De León';
+
